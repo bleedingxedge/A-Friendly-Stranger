1a87fa909abf8844b4d06dbfd3c9de139c1e87b5
Jeduan Cornejo <jeduan@gmail.com>
2014-05-05 14:32:05 -0500

    Adding tumblr support

--- routers/searchRouter.js
+++ routers/searchRouter.js
@@ -2,8 +2,9 @@
 var express   = require('express')
 var config    = require('../config')
 var instagram = require('instagram-node')
-var Twit      = require('twit')
 var Promise   = require('bluebird')
+var tumblr    = require('tumblr.js')
+var Twit      = require('twit')
 
 var router = express.Router()
 
@@ -13,16 +14,19 @@ var twitterClient = new Twit({
   access_token: '3367771-I8ccGU4UUTaH5OUV6Q3YlkQoC0Hk7VWMH7Z6CwbUQf',
   access_token_secret: 'Yj85eg4fuL4aDV8l6cJrYZ0l2iFvAJKw4JQ5gFe2AxiUf',
 })
+Promise.promisifyAll(twitterClient)
 
 var instagramClient = instagram.instagram()
-
 instagramClient.use({
   client_id: '885de22d7010427681562ab71cd81127',
   client_secret: '0a88e2d362fb4b23b1d5c6c4d93a8a34'
 })
-
-Promise.promisifyAll(twitterClient)
 Promise.promisifyAll(instagramClient)
+
+var tumblrClient = new tumblr.Client({
+  consumer_key: 'AgXEbbB61lQfHK9HfCXnDz0ZoNCN9HRobHHxJpbZ9gIf6RpgEt'
+})
+Promise.promisifyAll(tumblrClient)
 
 function getTweets() {
   var searchTerm = '#' + config.searchTerm
@@ -33,7 +37,7 @@ function getTweets() {
   })
 }
 
-function getInstagram() {
+function findInstagram() {
   return instagramClient.tag_media_recentAsync(config.searchTerm)
   .then(function (res) {
     // res -> [medias, pagination, limit]
@@ -41,13 +45,19 @@ function getInstagram() {
   })
 }
 
+function findTumblr() {
+  return tumblrClient.taggedAsync(config.searchTerm)
+}
+
 router.get('/', function(req, res, next) {
-  Promise.all([getTweets(), getInstagram()])
-  .spread(function (twitterData, instagramData) {
-    var statusLength = (twitterData.length || 0) + (instagramData.length || 0)
+  Promise.all([getTweets(), findInstagram(), findTumblr()])
+  .spread(function (twitterData, instagramData, tumblrData) {
+    var statusLength = (twitterData.length || 0) + (instagramData.length || 0) +
+    (tumblrData.length || 0)
     res.send({
       twitter: twitterData,
       instagram: instagramData,
+      tumblr: tumblrData,
       length: statusLength
     })
   }).catch(next)
