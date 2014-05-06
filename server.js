c46cee344a25c7696e567d89c978fa5b220d149e
Jeduan Cornejo <jeduan@gmail.com>
2014-05-05 23:02:05 -0500

    Final touches

--- server.js
+++ server.js
@@ -22,11 +22,9 @@ app.get('/', function(req, res) {
 })
 
 var getMedia = require('./lib/media')
-app.get('/search', function() {
-  getMedia().then(function(results){
-    results.length = results.twitter.length + results.instagram.length +
-      results.tumblr.length
-    res.send(results)
+app.get('/search', function(req, res, next) {
+  getMedia().then(function(items){
+    res.send(items)
   }).catch(next)
 })
 
@@ -44,11 +42,7 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
+    app.use(require('errorhandler')())
   });
 }
 
@@ -56,7 +50,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  console.log(err)
+  console.error(err)
   res.render('error', {
     message: err.message,
     error: {}
