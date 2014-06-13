7aefe28586b4e60d6acfaa4950943195e789a931
Jeduan Cornejo <jeduan@gmail.com>
2014-06-13 00:59:13 -0500

    Adding logger

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.set('views', __dirname + '/views')
 app.set('port', process.env.PORT || 3000)
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded())
+app.use(require('morgan')('dev'))
 // app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'))
 
